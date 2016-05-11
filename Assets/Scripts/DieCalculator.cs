ffadf08a7e0b7eed7f751a48ca4d40892a6c8b1b
duane <sandworm@gmail.com>
2016-05-11 16:46:17 -0700

    Added particle effect to dice on high roll

--- Assets/Scripts/DieCalculator.cs
+++ Assets/Scripts/DieCalculator.cs
@@ -5,6 +5,7 @@ public class DieCalculator : MonoBehaviour {
 
 	public Transform[] faces;
 	public TextMesh text;
+	public ParticleSystem particleSystem;
 
 	Rigidbody rb;
 	int dieValue = 0;
@@ -13,12 +14,15 @@ public class DieCalculator : MonoBehaviour {
 	void Awake()
 	{
 		rb = gameObject.GetComponent<Rigidbody> ();
+		//particleSystem = gameObject.GetComponent<ParticleSystem> ();
 		text.gameObject.SetActive (false);
 	}
 
 	void AnimateDieValue()
 	{
 		if (!dieValueHasAnimated) {
+			if (particleSystem && dieValue == faces.Length)
+				particleSystem.Emit (120);
 			iTween.ScaleFrom (text.gameObject, iTween.Hash ("x", 0.1f, "y", 0.1f, "time", 0.5f, "easetype", iTween.EaseType.easeOutElastic));
 			dieValueHasAnimated = true;
 		}
