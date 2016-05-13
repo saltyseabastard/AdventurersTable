a8c644dd95114b15cb4468b3c409a97e8fee3f8d
Fifield <sheldon.fifield@intel.com>
2016-05-13 11:24:15 -0700

    Test Commit for merging

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+
+	Transform mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var n = mainCamera.position - transform.position; 
+		transform.rotation = Quaternion.LookRotation( -n ); 
+	
+	}
+}
