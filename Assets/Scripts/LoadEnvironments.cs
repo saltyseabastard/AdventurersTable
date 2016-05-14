25624c5320dae284419abf157bb2235fac06762b
sdfifield <dfifield@gmail.com>
2016-05-13 23:30:05 -0700

    First crack at Stonehenge. Needs a better skybox.

--- Assets/Scripts/LoadEnvironments.cs
+++ Assets/Scripts/LoadEnvironments.cs
@@ -1,28 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LoadEnvironments : MonoBehaviour {
+public class LoadEnvironments : MonoBehaviour
+{
 
     private GameObject environment;
-    private string[] list = { "/Environments/BlackSmith","/Environments/BlackSmith2", "/Environments/Forest" };
-    public Material MaterialRef1;
-    public Material MaterialRef2;
+    public GameObject[] environ;
+    public Material[] MaterialRef;
 
-    // Use this for initialization
     void Start()
     {
 
-        foreach (string element in list)
+        foreach (GameObject go in environ)
         {
-            environment = GameObject.Find(element);
-            SetTargetInvisible(environment);
+            go.SetActive(false);
         }
-
-//        environment = GameObject.Find("/Environments/BlackSmith2");
-//        SetTargetInvisible(environment);
-
-        environment = GameObject.Find("/Environments/BlackSmith");
-        SetTargetVisible(environment);
+        environ[1].SetActive(true);
     }
 
     // Update is called once per frame
@@ -30,66 +23,39 @@ public class LoadEnvironments : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            SetTargetInvisible(environment);
-            environment = GameObject.Find("/Environments/BlackSmith2");
-            RenderSettings.skybox = MaterialRef2;
-            SetTargetVisible(environment);
+            foreach (GameObject go in environ)
+            {
+                go.SetActive(false);
+            }
+            environ[0].SetActive(true);
+            RenderSettings.skybox = MaterialRef[0];
         }
         if (Input.GetKeyDown(KeyCode.X))
         {
-            SetTargetInvisible(environment);
-            environment = GameObject.Find("/Environments/BlackSmith");
-            RenderSettings.skybox = MaterialRef2;
-            SetTargetVisible(environment);
+            foreach (GameObject go in environ)
+            {
+                go.SetActive(false);
+            }
+            environ[1].SetActive(true);
+            RenderSettings.skybox = MaterialRef[1];
         }
         if (Input.GetKeyDown(KeyCode.C))
         {
-            SetTargetInvisible(environment);
-            environment = GameObject.Find("/Environments/Forest");
-            RenderSettings.skybox = MaterialRef1;
-            SetTargetVisible(environment);
+            foreach (GameObject go in environ)
+            {
+                go.SetActive(false);
+            }
+            environ[2].SetActive(true);
+            RenderSettings.skybox = MaterialRef[2];
         }
-    }
-
-    //Turns off an environment
-    void SetTargetInvisible(GameObject Target)
-    {
-        Component[] gameObjects = Target.GetComponentsInChildren(typeof(Renderer));
-        Light[] lights = Target.GetComponentsInChildren<Light>(true);
-        Collider[] colliders = Target.GetComponentsInChildren<Collider>(true);
-        foreach (Component i in gameObjects)
+        if (Input.GetKeyDown(KeyCode.V))
         {
-            Renderer scene = (Renderer)i;
-            scene.enabled = false;
-        }
-        foreach (Light i in lights)
-        {
-            i.enabled = false;
-        }
-        foreach (Collider i in colliders)
-        {
-            i.enabled = false;
-        }
-    }
-
-    //Turns on an environment
-    void SetTargetVisible(GameObject Target)
-    {
-        Component[] gameObjects = Target.GetComponentsInChildren(typeof(Renderer));
-        Light[] lights = Target.GetComponentsInChildren<Light>(true);
-        Collider[] colliders = Target.GetComponentsInChildren<Collider>(true);
-        foreach (Component i in gameObjects)
-        {
-            Renderer scene = (Renderer)i;
-            scene.enabled = true;
-        }
-        foreach (Light i in lights)
-        {
-            i.enabled = true;
-        }
-        foreach (Collider i in colliders)
-        {
-            i.enabled = true;
+            foreach (GameObject go in environ)
+            {
+                go.SetActive(false);
+            }
+            environ[3].SetActive(true);
+            RenderSettings.skybox = MaterialRef[3];
         }
     }
 }
