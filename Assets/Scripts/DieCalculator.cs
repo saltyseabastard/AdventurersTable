582b3d876381fa61b93413baa378c05bc82ce480
h3teroerectus <sandworm@gmail.com>
2016-05-16 14:41:35 -0700

    Dice now have a 7 sec time limit, then die animatedly

--- Assets/Scripts/DieCalculator.cs
+++ Assets/Scripts/DieCalculator.cs
@@ -5,12 +5,14 @@ public class DieCalculator : MonoBehaviour {
 
 	public Transform[] faces;
 	public TextMesh text;
-	public ParticleSystem particleSystem;
+	public ParticleSystem highRollSparks;
+	public ParticleSystem deathFlicker;
 
 	Rigidbody rb;
 	int dieValue = 0;
 	bool dieValueHasAnimated = false;
 	Transform mainCamera;
+	bool countDownHasStarted = false;
 
 	void Awake()
 	{
@@ -19,11 +21,28 @@ public class DieCalculator : MonoBehaviour {
 		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").transform;
 	}
 
+	IEnumerator CountdownToExtinction()
+	{
+		countDownHasStarted = true;
+		yield return new WaitForSeconds(7);
+
+		if (countDownHasStarted)
+		{
+			iTween.ScaleTo (gameObject, iTween.Hash ("scale", Vector3.zero, "time", 0.5f, "easetype", iTween.EaseType.easeInElastic));
+			yield return new WaitForSeconds (0.35f);
+			if (deathFlicker)
+				deathFlicker.Emit (1);
+			yield return new WaitForSeconds (0.5f);
+			Destroy (gameObject);
+		}
+	}
+
 	void AnimateDieValue()
 	{
 		if (!dieValueHasAnimated) {
-			if (particleSystem && dieValue == faces.Length)
-				particleSystem.Emit (120);
+			if (GetComponent<ParticleSystem>() && dieValue == faces.Length)
+				GetComponent<ParticleSystem>().Emit (120);
+
 			iTween.ScaleFrom (text.gameObject, iTween.Hash ("x", 0.1f, "y", 0.1f, "time", 0.5f, "easetype", iTween.EaseType.easeOutElastic));
 			dieValueHasAnimated = true;
 		}
@@ -64,9 +83,16 @@ public class DieCalculator : MonoBehaviour {
 			RotateTextToFaceCamera ();
 			CalculateDieValue ();
 			AnimateDieValue ();
+			if (!countDownHasStarted)
+			{
+				StartCoroutine ("CountdownToExtinction");
+			}
+				
+
 		} 
 		else 
 		{
+			countDownHasStarted = false;
 			text.gameObject.SetActive (false);
 			dieValueHasAnimated = false;
 		}
