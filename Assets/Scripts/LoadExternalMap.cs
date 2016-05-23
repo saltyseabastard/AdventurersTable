0ca460c3142928125571ccfbd57927cc72fa208e
h3teroerectus <sandworm@gmail.com>
2016-05-23 15:13:17 -0700

    Added ability to add user maps, just drop a file in the Maps directory. Hardcoded for now.

---
+++ Assets/Scripts/LoadExternalMap.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadExternalMap : MonoBehaviour {
+
+	string path;
+	string filename = "licktoad.jpg";
+	Renderer map;
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		map = GetComponent<Renderer> ();
+
+		//populate path
+		path = Application.dataPath;
+
+		if (Application.platform == RuntimePlatform.OSXPlayer)
+		{
+			path += "/../../";
+		} 
+		else if (Application.platform == RuntimePlatform.WindowsPlayer)
+		{
+			path += "/../";
+		} 
+		else
+		{
+			path +=	"/../Maps/";
+		}
+
+		print (path);
+
+		WWW www = new WWW("file://" + path + filename);
+		yield return www;
+
+		print (www.bytes.Length);
+
+		Texture2D texture = new Texture2D(2, 2);
+		texture.LoadImage(www.bytes);
+
+		map.material.mainTexture = texture;
+	}
+	
+
+}
--- Assets/Scripts/Pallette.cs
+++ Assets/Scripts/Pallette.cs
@@ -3,51 +3,97 @@ using System.Collections;
 
 public class Pallette : MonoBehaviour {
 
+	public LoadEnvironments envLoader;
 	public GameObject[] diceButtons;
 	public GameObject[] dicePrefabs;
 
 	//the pickupObject script on the character controller
 	public PickupObject pickupObject;
+	int faceUp = 0;
 
 	void SpawnDice()
 	{
 		if(Input.GetKeyDown (KeyCode.Alpha1))
 		{
-			GameObject d4 = (GameObject) Instantiate(dicePrefabs[0], new Vector3 (transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+			GameObject d4 = (GameObject) Instantiate(dicePrefabs[0], transform.position, transform.rotation);
 			pickupObject.AddObjectToHand(d4);
 			//TODO: animate button
 		}
 		if(Input.GetKeyDown (KeyCode.Alpha2))
 		{
-			GameObject d6 = (GameObject) Instantiate(dicePrefabs[1], new Vector3 (transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+			GameObject d6 = (GameObject) Instantiate(dicePrefabs[1], transform.position, transform.rotation);
 			pickupObject.AddObjectToHand(d6);
 		}
 		if(Input.GetKeyDown (KeyCode.Alpha3))
 		{
-			GameObject d8 = (GameObject) Instantiate(dicePrefabs[2], new Vector3 (transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+			GameObject d8 = (GameObject) Instantiate(dicePrefabs[2], transform.position, transform.rotation);
 			pickupObject.AddObjectToHand(d8);
 		}
 		if(Input.GetKeyDown (KeyCode.Alpha4))
 		{
 			//TODO: Multiple colors of dice here
-			GameObject d10 = (GameObject) Instantiate(dicePrefabs[3], new Vector3 (transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+			GameObject d10 = (GameObject) Instantiate(dicePrefabs[3], transform.position, transform.rotation);
 			pickupObject.AddObjectToHand(d10);
 		}
 		if(Input.GetKeyDown (KeyCode.Alpha5))
 		{
-			GameObject d12 = (GameObject) Instantiate(dicePrefabs[4], new Vector3 (transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+			GameObject d12 = (GameObject) Instantiate(dicePrefabs[4], transform.position, transform.rotation);
 			pickupObject.AddObjectToHand(d12);
 		}
 		if(Input.GetKeyDown (KeyCode.Alpha6))
 		{
-			GameObject d20 = (GameObject) Instantiate(dicePrefabs[5], new Vector3 (transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+			GameObject d20 = (GameObject) Instantiate(dicePrefabs[5], transform.position, transform.rotation);
 			pickupObject.AddObjectToHand(d20);
+		}
+	}
+
+	void AnimateButton(GameObject button)
+	{
+		//iTween.MoveFrom(
+	}
+
+	void SwapEnvironments()
+	{
+		if(Input.GetKeyDown (KeyCode.Alpha1))
+		{
+			envLoader.ChangeEnvironment (LoadEnvironments.Environment.Blacksmith);
+			//TODO: animate button
+		}
+		if(Input.GetKeyDown (KeyCode.Alpha2))
+		{
+			envLoader.ChangeEnvironment (LoadEnvironments.Environment.Forest);
+		}
+		if(Input.GetKeyDown (KeyCode.Alpha3))
+		{
+			envLoader.ChangeEnvironment (LoadEnvironments.Environment.Stonehenge);
+		}
+	}
+
+	void RotatePallette()
+	{
+		if (Input.GetKeyDown (KeyCode.L))
+		{
+			//transform.Rotate(new Vector3 (0, 90, 0), Space.Self);
+			iTween.RotateBy(gameObject, iTween.Hash("y", -0.25f, "islocal", true, "time", 0.25f, "easetype", iTween.EaseType.easeOutQuad));
+
+			faceUp = faceUp == 3 ? 0 : faceUp + 1;
 		}
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-		SpawnDice ();
+		RotatePallette ();
+
+		switch (faceUp)
+		{
+		case 0: 
+			SpawnDice ();
+			break;
+		
+		case 1:
+			SwapEnvironments ();
+			break;
+		}
 	}
 }
