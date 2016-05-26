93ada45ce6712dbfb41ffe7f0f649c46da37011c
sdfifield <dfifield@gmail.com>
2016-05-26 10:26:12 -0700

    Added Simple Networking. Known issues: Initial Dice point at Main Camera. Dice Spawning is not networked, so dice spawn for all characters, but can only be seen by the local player. Dice always point at the host. Table Map can only be seen by the host.

---
+++ Assets/Scripts/NetworkedPlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkedPlayerScript : NetworkBehaviour
+{
+    public UnityStandardAssets.Characters.FirstPerson.FirstPersonController fpsController;
+    public Camera fpsCamera;
+    public AudioListener audioListener;
+    public PickupObject pickupScript;
+    //public ShootingScript shootingScript;
+    //public GunMaterialSwitcher gunMaterialSwitcher;
+
+    Renderer[] renderers;
+
+    void Start()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        fpsController.enabled = true;
+        fpsCamera.enabled = true;
+        audioListener.enabled = true;
+        pickupScript.enabled = true;
+        //shootingScript.enabled = true;
+        //gunMaterialSwitcher.SwitchMaterial(true);
+
+        gameObject.name = "LOCAL Player";
+        base.OnStartLocalPlayer();
+    }
+}
