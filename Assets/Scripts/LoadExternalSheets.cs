c391f4855fdc24acabdc5b520a2df98ae3a622d4
Duane <sandworm@gmail.com>
2016-05-29 13:57:00 -0700

    Clipboard and mini can now be picked up, disabled quads

---
+++ Assets/Scripts/LoadExternalSheets.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadExternalSheets : MonoBehaviour {
+
+	string path;
+	string filename = "duane.jpg";
+	Renderer sheet;
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		sheet = GetComponent<Renderer> ();
+
+		//populate path
+		path = Application.dataPath;
+
+		if (Application.platform == RuntimePlatform.OSXPlayer)
+		{
+			path += "/../../";
+		} 
+		else if (Application.platform == RuntimePlatform.WindowsPlayer)
+		{
+			path += "/../";
+		} 
+		else
+		{
+			path +=	"/../Sheets/";
+		}
+
+		WWW www = new WWW("file://" + path + filename);
+		yield return www;
+
+		Texture2D texture = new Texture2D(2, 2);
+		texture.LoadImage(www.bytes);
+
+		sheet.material.mainTexture = texture;
+	}
+	
+
+}
